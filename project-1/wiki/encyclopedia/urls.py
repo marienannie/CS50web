d3c11d406a66677d52e5efb2848882bc6be2cb65
marienannie <cockroachan@outlook.com>
2025-07-22 00:02:44 -0700

    Add some style for 'Home' and 'Create New Page' Pages

--- project-1/wiki/encyclopedia/urls.py
+++ project-1/wiki/encyclopedia/urls.py
@@ -3,5 +3,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("", views.index, name="index")
+    path("", views.index, name="index"),
+    path("encyclopedia/add.html", views.add, name="add"),
+    path("encyclopedia/<str:title>", views.entry, name="entry")
 ]
--- project-1/wiki/encyclopedia/views.py
+++ project-1/wiki/encyclopedia/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-
+import markdown2
 from . import util
 
 
@@ -8,3 +8,16 @@ def index(request):
         "entries": util.list_entries()
     })
 
+def add(request):
+    return render(request, "encyclopedia/add.html")
+
+def entry(request, title):
+    content = util.get_entry(title)
+    if content is None:
+        return render(request, "encyclopedia/error.html", {
+            "message": f'The "{title}" entry does not exist.'
+        })
+    return render(request, "encyclopedia/entry.html"), {
+        "title": title,
+        "content": markdown2.markdown(content)
+    }
